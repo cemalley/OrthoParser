d397a491a019024d8d937957eb621be0e6c6474c
cemalley <claire@x70o.local>
2015-03-19 13:36:14 -0500

    Summary stats CSV
    Gathers stats from orthogroup FASTA files into a handy CSV with group
    IDs, number of genes in group, number of species in group, and the
    average number of genes per species in group.

---
+++ o-f-csv.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+### ORTHOPARSER 2015 ###
+#Claire Malley March 18 2015
+#Gather stats into CSV using OrthoParser output
+use warnings;
+use List::Util qw(sum);
+use Text::CSV;
+
+my @opFiles = glob "OG*.fa";
+my @CSV0 = ();
+my @CSV1 = ();
+my @CSV2 = ();
+my @CSV3 = ();
+my $CSV0ref = \@CSV0;
+my $CSV1ref = \@CSV1;
+my $CSV2ref = \@CSV2;
+my $CSV3ref = \@CSV3;
+
+my @header= ('GroupId', 'GroupSize', 'Species', 'GenesPerSpecies');
+my $headerref = \@header;
+
+my $csv = Text::CSV->new ( { binary => 1, sep_char => ',', eol    => "\n"} ) or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
+open $topcsv, ">:encoding(utf8)", "OrthoFinderStats.csv" or die "OrthoFinderStats.csv: $!";
+
+$csv->print($topcsv, $headerref);
+
+close $topcsv;
+
+
+foreach my $opFiles (@opFiles){
+    my @genesInGroup = ();
+    open(my $infh, '<', $opFiles) or die $!;
+        while(<$infh>){
+            my $line = $_ unless ($_ eq "\n");
+            chomp $line;
+            if(/^>/){
+                my $geneInGroup = substr $line, 1, 5;
+                push @genesInGroup, $geneInGroup;
+            }
+        }
+    my %counts;
+    $counts{$_}++ for @genesInGroup;
+    my $groupSize = $#genesInGroup + 1;
+    my $groupName = substr $opFiles, 0, 8;
+    my $specIncl = scalar keys %counts;
+    my $avgGenesPerSpec = (sum values %counts)/($specIncl);
+    push @CSV0, $groupName; #aka group ID
+    push @CSV1, $groupSize;
+    push @CSV2, $specIncl;
+    push @CSV3, $avgGenesPerSpec;
+
+my @bulk = (@CSV0, @CSV1, @CSV2, @CSV3);
+my $bulk = \@bulk;
+
+open $bulkcsv, ">>:encoding(utf8)", "OrthoFinderStats.csv" or die "OrthoFinderStats.csv: $!";
+
+$csv->print($bulkcsv, $bulk);
+
+close $bulkcsv;
+
+@CSV0=();
+@CSV1=();
+@CSV2=();
+@CSV3=();
+
+close $infh;
+}
+
+exit;
